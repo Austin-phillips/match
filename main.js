7d99befbe2e08d0ac047706282a33e063ccce596
Austin Phillips <phillips.austin51@gmail.com>
2017-11-15 21:08:09 -0700

    Add switch

---
+++ main.js
@@ -0,0 +1,147 @@
+// get cards to equal something
+
+// get cards that eqaul the same to dissapear matches
+
+$(document).ready( function() {
+
+var cardValue1 = [0]
+var cardValue2 = [0]
+
+  
+// Card 1
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button1":
+      cardName = "Ruby"
+      break
+
+
+
+    }
+    $('#button1').text(cardName)
+  })
+  // Card2
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button2":
+      cardName = "C++"
+      break
+    }
+    $('#button2').text(cardName)
+  })
+  // Card3
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button3":
+      cardName = "HTML"
+      break
+    }
+    $('#button3').text(cardName)
+  })
+  // Card4
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button4":
+      cardName = "JavaScript"
+      break
+    }
+    $('#button4').text(cardName)
+  })
+  // Card5
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button5":
+      cardName = "Python"
+      break
+    }
+    $('#button5').text(cardName)
+  })
+  // Card6
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button6":
+      cardName = "CSS"
+      break
+    }
+    $('#button6').text(cardName)
+  })
+  // Card7
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button7":
+      cardName = "CSS"
+      break
+    }
+    $('#button7').text(cardName)
+  })
+  // Card8
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button8":
+      cardName = "Ruby"
+      break
+    }
+    $('#button8').text(cardName)
+  })
+  // Card9
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button9":
+      cardName = "JavaScript"
+      break
+    }
+    $('#button9').text(cardName)
+  })
+  // Card10
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button10":
+      cardName = "HTML"
+      break
+    }
+    $('#button10').text(cardName)
+  })
+  // Card11
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button11":
+      cardName = "C++"
+      break
+    }
+    $('#button11').text(cardName)
+  })
+  // Card12
+  $('.btn-large').on('click', function() {
+    var cardName
+
+    switch(this.id) {
+      case "button12":
+      cardName = "Python"
+      break
+    }
+    $('#button12').text(cardName)
+  })
+})
+
